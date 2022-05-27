74c57ac7ecafcf2ecada65146c336d2e8f7ce5c3
Khizer Younas <m.khizeryounas@gmail.com>
2022-05-27 02:30:20 -0700

    feat: added ddt in test cases

--- pro_assignment/utils/tests/test_api_response.py
+++ pro_assignment/utils/tests/test_api_response.py
@@ -1,24 +1,24 @@
 from django.test import SimpleTestCase
 from ..api_response import ok, error, ERROR_CODES
 import logging
+import ddt
 
 logger = logging.getLogger(__name__)
 
 
+@ddt.ddt
 class TestApiResponse(SimpleTestCase):
     def test_should_return_http_200_status_code(self):
         """Test ‘ok’ should return a 200 status code"""
         response = ok()
         self.assertEqual(response.status_code, 200)
 
-    def test_should_return_http_400_status_code(self):
-        """Test ‘error’ should return a 400 status code"""
-        response = error()
-        self.assertEqual(response.status_code, 400)
-
-    def test_should_return_passed_error_status_code(self):
-        """Test ‘error’ should return a provided error status code"""
-        status_code = 429  # Too Many Requests
+    @ddt.data(
+        400, 401, 429
+    )
+    def test_should_return_http_error_status_code(self, status_code):
+        """Test ‘error’ should return a error status code"""
         response = error(status=status_code)
         self.assertEqual(response.status_code, status_code)
-        self.assertEqual(response.data["message"], ERROR_CODES[status_code])
+        self.assertEqual(response.data.get('message', None),
+                         ERROR_CODES[status_code])
--- pro_assignment/utils/tests/test_query_filter.py
+++ pro_assignment/utils/tests/test_query_filter.py
@@ -1,3 +1,4 @@
+import ddt
 from django.test import SimpleTestCase
 import logging
 from ..query_filter import rename_operation_name, tokenize, array_to_tree
@@ -6,66 +7,8 @@ from django.db.models import Q
 logger = logging.getLogger(__name__)
 
 
-class TestQueryFilter(SimpleTestCase):
-    # Set base params to reuse the same query and parsed_query
-    query = 'EQUAL(id, "first-post")'
-    parsed_query = {'operator': 'EQUAL',
-                    'param1': 'id', 'param2': 'first-post'}
-
-    def test_should_rename_operation_name(self):
-        """Test rename_operation_name function"""
-        self.assertEqual(rename_operation_name("LESS_THAN"), "lt")
-        self.assertEqual(rename_operation_name("GREATER_THAN"), "gt")
-        self.assertEqual(rename_operation_name("EQUAL"), "eq")
-
-    def test_should_tokenize_query(self):
-        """Test Tokenizes query"""
-        self.assertListEqual(tokenize(self.query), [self.parsed_query])
-        self.assertListEqual(tokenize(f'NOT({self.query})'), [
-            {'operator': 'NOT'},
-            self.parsed_query
-        ])
-        self.assertListEqual(tokenize(f'OR({self.query}, NOT({self.query}))'), [
-            {'operator': 'OR'},
-            self.parsed_query,
-            {'operator': 'NOT'},
-            self.parsed_query
-        ])
-
-    def test_should_tokenize_raise_exception_with_invalid_query(self):
-        """Test raises exception with invalid query"""
-        with self.assertRaises(Exception):
-            # The passed query is not valid because the paranethesis are not balanced
-            tokenize(f'{self.query})')
-
-    def test_should_array_to_tree_return_filter_tree(self):
-        """Test converts operator array to query tree"""
-        # Construct Q tree for EQUAL operator
-        self.assertEqual(array_to_tree(
-            array=[self.parsed_query], filter=Q()), Q(id="first-post"))
-
-        # Checking for OR and NOT operators
-        self.assertEqual(
-            array_to_tree(array=[
-                {'operator': 'OR'},
-                self.parsed_query,
-                {'operator': 'NOT'},
-                self.parsed_query], filter=Q()
-            ), self.construct_actual_filter(conn_type=Q.OR)
-        )
-
-        # Checking for AND and NOT operators
-        self.assertEqual(
-            array_to_tree(array=[
-                {'operator': 'AND'},
-                self.parsed_query,
-                {'operator': 'NOT'},
-                self.parsed_query], filter=Q()
-            ), self.construct_actual_filter(conn_type=Q.AND)
-        )
-
-    def construct_actual_filter(self, conn_type=Q.AND):
-        """Construct actual Q filter
+def construct_actual_filter(conn_type=Q.AND):
+    """Construct actual Q filter
 
         Args:
             conn_type (str, optional): Connection type. Defaults to Q.AND.
@@ -73,7 +16,93 @@ class TestQueryFilter(SimpleTestCase):
         Returns:
             Q: Query filter
         """
-        filter = Q()
-        filter.add(Q(id="first-post"), conn_type=conn_type)
-        filter.add(~Q(id="first-post"), conn_type=conn_type)
-        return filter
+    filter = Q()
+    filter.add(Q(id="first-post"), conn_type=conn_type)
+    filter.add(~Q(id="first-post"), conn_type=conn_type)
+    return filter
+
+
+@ddt.ddt
+class TestQueryFilter(SimpleTestCase):
+    # Set base params to reuse the same query and parsed_query
+    query = 'EQUAL(id, "first-post")'
+    parsed_query = {'operator': 'EQUAL',
+                    'param1': 'id', 'param2': 'first-post'}
+
+    @ddt.data(
+        {'input': 'LESS_THAN', 'actual': 'lt'},
+        {'input': 'GREATER_THAN', 'actual': 'gt'},
+        {'input': 'EQUAL', 'actual': 'eq'},
+    )
+    def test_should_rename_operation_name(self, param):
+        """Test rename_operation_name function"""
+        self.assertEqual(rename_operation_name(
+            param['input']), param['actual']
+        )
+
+    @ddt.data(
+        {
+            'input': query,
+            'actual': [
+                parsed_query
+            ]
+        },
+        {
+            'input': f'NOT({query})',
+            'actual': [
+                {'operator': 'NOT'},
+                parsed_query
+            ]
+        },
+        {
+            'input': f'OR({query}, NOT({query}))',
+            'actual': [
+                {'operator': 'OR'},
+                parsed_query,
+                {'operator': 'NOT'},
+                parsed_query
+            ]
+        },
+    )
+    def test_should_tokenize_query(self, params):
+        """Test Tokenizes query"""
+        self.assertListEqual(tokenize(params['input']), params['actual'])
+
+    def test_should_tokenize_raise_exception_with_invalid_query(self):
+        """Test raises exception with invalid query"""
+        with self.assertRaises(Exception):
+            # The passed query is not valid because the paranethesis are not balanced
+            tokenize(f'{self.query})')
+
+    @ddt.data(
+        {
+            'input': [parsed_query],
+            'actual': Q(id="first-post")
+        },
+        # Checking for OR and NOT operators
+        {
+            'input': [
+                {'operator': 'OR'},
+                parsed_query,
+                {'operator': 'NOT'},
+                parsed_query
+            ],
+            'actual': construct_actual_filter(conn_type=Q.OR)
+        },
+        # Checking for AND and NOT operators
+        {
+            'input': [
+                {'operator': 'AND'},
+                parsed_query,
+                {'operator': 'NOT'},
+                parsed_query
+            ],
+            'actual': construct_actual_filter(conn_type=Q.AND)
+        }
+    )
+    def test_should_array_to_tree_return_filter_tree(self, params):
+        """Test converts operator array to query tree"""
+        self.assertEqual(
+            array_to_tree(array=params['input'], filter=Q()),
+            params['actual']
+        )
