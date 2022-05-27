9accb8a17b7991c6d05e3ef5d93a494e546ce310
Khizer Younas <m.khizeryounas@gmail.com>
2022-05-27 02:36:41 -0700

    feat: added service test

---
+++ pro_assignment/apps/store/tests/test_post_service.py
@@ -0,0 +1,7 @@
+from django.test import SimpleTestCase
+
+
+class TestApiResponse(SimpleTestCase):
+    def test_should_create_post(self):
+        """Test should create a post"""
+        self.assertEqual(True, True)
